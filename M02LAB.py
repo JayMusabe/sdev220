81b92812599a10b073bddd8bae72a9f4f136a9d8
JayMusabe <jmusabe@ivytech.edu>
2022-11-07 00:09:41 -0500

    Added M02ProgrammingAssignments file

--- M02LAB.py
+++ M02LAB.py
@@ -1,11 +1,31 @@
+#Jules Musabe
+#File name: M02Lab.py
+#The app will accept student names and GPA and test to see if they made the Dean's List or 
+# the Honors Roll
+
+
+#set a variable to allow users to try again
 
 TryAgain = 'Y'
 
+#The app will run as long as the TryAgain variable is 'Y'
 while TryAgain  == 'Y':
+    
+#Get the student's last name
+
     LastName = str ( input ("Please enter the student's last name: "))
+
+#Exit if the last name is ZZZ
+
     if LastName != "ZZZ":
+        
+#Get the student's first name and GPA
+
         FirstName =  input ("Please enter the student's first name: ")
         StudentGPA = float(input ("Please enter the student's GPA: "))
+        
+#Process the GPA, print the results, and ask the user if they want to check another student
+
         if StudentGPA >= 3.5:
             print ("Dear ", FirstName, " ", LastName, ", congratulations! You Have made the Dean's List! ")
             TryAgain = input ("Would you like to check another student? (Y/N) ")
@@ -17,7 +37,9 @@ while TryAgain  == 'Y':
         else:
             print ("Dear ", FirstName, " ", LastName, ", keep working hard. You haven't made Dean's List or the Honor Roll.")
             TryAgain = input ("Would you like to check another student? (Y/N) ")
+ 
+ #inform the user that they quit the process and ask them if they want to start over.
     
     else:
-        print ("Invalid input")
-        TryAgain = input ("Would you like to check another student? (Y/N) ")
+        print ("You aborted the process.")
+        TryAgain = input ("Would you like to start over? (Y/N) ")
